162ad56359b8c6734bb19172454bb4e56ca1b4d6
JunPark <jjoon0306@naver.com>
2022-07-25 14:20:10 +0900

    [FE] login page add

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/index";
+import LoginForm from "./pages/Login/index";
+import { doLogin } from "./api/member";
 
 function App() {
+  const [user, setUser] = useState();
+  const authenticated = user != null;
+
+  const login = async ({ id, password }) => {
+    try {
+      await doLogin(
+        {
+          id: { id },
+          password: { password },
+        },
+        () =>
+          setUser({
+            id: { id },
+            password: { password },
+          }),
+        () => console.log("로그인 실패")
+      );
+    } catch (error) {
+      console.log("로그인 실패");
+    }
+  };
+  //const logout = () => setUser(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            authenticated ? (
+              <Home />
+            ) : (
+              <LoginForm authenticated={authenticated} login={login} />
+            )
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
