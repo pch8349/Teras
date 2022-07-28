898dd34db4007818f21719745d61cca2c3105c62
chan449 <chan449@naver.com>
2022-07-28 14:00:41 +0900

    main

---
+++ figma-test/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import { Student_Main } from "./Student_Main";
+
+function App() {
+  document.body.style.backgroundColor = "#f5f5f5";
+  document.body.style.margin = "0";
+  // eslint-disable-next-line react/jsx-pascal-case
+  return <Student_Main />;
+}
+
+export default App;
