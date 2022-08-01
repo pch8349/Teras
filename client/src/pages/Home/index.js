9fcfeebe1752d00c57785454ce532d05f588e7af
박영준 <jjoon0306@naver.com>
2022-08-01 17:41:49 +0900

    [FE] mui timetable add

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,14 +1,23 @@
 import React from "react";
+import "./home.css";
+import TimeTable from "./components/Timetable/Timetable";
 
 function Home() {
   return (
-    <>
-      <h1>Home</h1>
-      <p>Welcome home!</p>
-      <button onClick={() => (window.location.href = "/classroom")}>
-        강의실로 이동
-      </button>
-    </>
+    <div className="mainFlexContainer">
+      <div className="mainGridContainer">
+        <div className="header"></div>
+        <div className="sideBarContainer">
+          <div className="profileContainer"></div>
+          <div className="timeTableContainer">
+            <TimeTable
+              rows={["수학", "영어", "국어", "진로", "화학I", "체육", "도덕"]}
+            />
+          </div>
+        </div>
+        <div className="dashboardContainer"></div>
+      </div>
+    </div>
   );
 }
 
