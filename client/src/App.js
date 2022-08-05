54dccb76216618410598ab1bcf61f7c35fc2cc7f
chan449 <chan449@naver.com>
2022-08-05 11:03:44 +0900

    [FE] Login/SignUp

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import "./App.css";
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/index";
-import LoginForm from "./pages/Login/index";
-import RouteGuard from "./pages/RouteGuard";
+import UserLogin from "./pages/Login/UserLogin";
+import SignUp from "./pages/Login/SignUp";
+import SignUpFin from "./pages/Login/SignUpFin";
 import Classroom from "./pages/Classroom/index";
 
 function App() {
@@ -17,10 +18,12 @@ function App() {
         <Route
           path="/"
           element={
-            localStorage.getItem("accessToken") ? <Home /> : <LoginForm />
+            localStorage.getItem("accessToken") ? <Home /> : <UserLogin />
           }
         />
-        <Route path="/login" element={<LoginForm />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signupfin" element={<SignUpFin />} />
         <Route path="/classroom" element={<Classroom />} />
       </Routes>
     </Router>
--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,7 +1,19 @@
 import { apiInstance } from "./index";
+import axios from "axios";
 
 const api = apiInstance();
 
 export async function doLogin(user, success, fail) {
   await api.post(`/auth/login`, JSON.stringify(user)).then(success).catch(fail);
 }
+
+export async function signUp(user, success, fail){
+  await api.post('/users', JSON.stringify(user)).then(success).catch(fail);
+}
+
+export const getSchool = async (params, success, fail) => {
+  return await axios
+    .get("https://open.neis.go.kr/hub/schoolInfo", { params })
+    .then(success)
+    .catch(fail);
+};
